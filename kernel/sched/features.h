d2df16996364671c0f81e7e105aab966a6653352
Tyler Nijmeh <tylernij@gmail.com>
2021-09-08 14:51:17 +0800

    sched: Do not reduce perceived CPU capacity while idle
    CPUs that are idle are excellent candidates for latency sensitive or
    high-performance tasks. Decrementing their capacity while they are idle
    will result in these CPUs being chosen less, and they will prefer to
    schedule smaller tasks instead of large ones. Disable this.
    
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>
    Signed-off-by: ryyaap. (unlstedd) <rktdnt@proton.me>
    Signed-off-by: HeroBuxx <me@herobuxx.me>
    Signed-off-by: Ardia Kun <ardiayogabekti@gmail.com>

--- kernel/sched/features.h
+++ kernel/sched/features.h
@@ -44,8 +44,8 @@
 /*
  * Decrement CPU capacity based on time not spent running tasks
  */
-#define SCHED_FEAT_NONTASK_CAPACITY 1
 
+#define SCHED_FEAT_NONTASK_CAPACITY 0
 /*
  * Queue remote wakeups on the target CPU and process them
  * using the scheduler IPI. Reduces rq->lock contention/bounces.
