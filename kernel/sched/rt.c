655a33c547ef855f3f4454e37c4cc772cdbedd97
kondors1995 <normandija1945@gmail.com>
2023-10-09 19:20:48 +0300

    Revert "sched: Never migrate tasks upon execve()"
    As per Emanuel & Sultan recomendation
    
    https://t.me/sultanskernel/956122
    
    This reverts commit 230a5b636c989303173b9c125fabe39e6edfc7e5.
    
    Signed-off-by: Yahya Wessam <yahyawessam2002@gmail.com>

--- kernel/sched/rt.c
+++ kernel/sched/rt.c
@@ -1507,8 +1507,8 @@ select_task_rq_rt(struct task_struct *p, int cpu, int sd_flag, int flags,
 	bool may_not_preempt;
 
 	/* For anything but wake ups, just return the task_cpu */
-	if (sd_flag == SD_BALANCE_EXEC)
-		return smp_processor_id();
+	if (sd_flag != SD_BALANCE_WAKE && sd_flag != SD_BALANCE_FORK)
+		goto out;
 
 	rq = cpu_rq(cpu);
 
@@ -1579,6 +1579,7 @@ select_task_rq_rt(struct task_struct *p, int cpu, int sd_flag, int flags,
 	}
 	rcu_read_unlock();
 
+out:
 	return cpu;
 }
 
