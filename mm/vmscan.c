4c05cda0c5045bfa247efbe83d2e4ebbc98a569e
Yuan Si <do4suki@gmail.com>
2024-04-04 00:53:44 +0800

    mm/vmscan: Bump swappiness
    Change-Id: I19e4a6e2566e2d6f96ce84045f240f789c2d2f15
    Signed-off-by: Yuan Si <do4suki@gmail.com>
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>
    Signed-off-by: Yahya Wessam <yahyawessam2002@gmail.com>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -189,7 +189,7 @@ static u64 workingset_protection_prev_totalram __read_mostly = 0;
 /*
  * From 0 .. 200.  Higher means more swappy.
  */
-int vm_swappiness = 10;
+int vm_swappiness = 160;
 /*
  * The total number of pages which are beyond the high watermark within all
  * zones.
