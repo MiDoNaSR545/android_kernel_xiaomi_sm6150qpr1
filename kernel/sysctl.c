cb2bbdbbc7cccb7282f116a24cb6d9b7b791257a
Flopster101 <nahuelgomez329@gmail.com>
2025-05-05 17:10:28 -0300

    kernel: sysctl: add init protection for sched_child_runs_first
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>
    Signed-off-by: Yahya Wessam <yahyawessam2002@gmail.com>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -353,6 +353,15 @@ int dummy_sched_boost_handler(struct ctl_table *table, int write,
 }
 #endif
 
+int proc_dointvec_wrapper(struct ctl_table *table, int write, void *buffer,
+ 			  size_t *lenp, loff_t *ppos)
+{
+	if (task_is_booster(current))
+		return 0;
+
+	return proc_dointvec(table, write, buffer, lenp, ppos);
+}
+
 static struct ctl_table kern_table[] = {
 	{
 		.procname	= "sched_child_runs_first",
@@ -367,7 +376,7 @@ static struct ctl_table kern_table[] = {
 		.data           = &sysctl_preemptoff_tracing_threshold_ns,
 		.maxlen         = sizeof(unsigned int),
 		.mode           = 0644,
-		.proc_handler   = proc_dointvec,
+		.proc_handler   = proc_dointvec_wrapper,
 	},
 #endif
 #if defined(CONFIG_IRQSOFF_TRACER) && defined(CONFIG_PREEMPTIRQ_EVENTS) && \
