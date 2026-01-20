b94634e58ecac19f044dc920e0900eb3084506f0
Yahya Wessam <yahyawessam2002@gmail.com>
2026-01-20 18:49:12 +0200

    Revert "msm: kgsl: Use kthread instead of workqueue for event work"
    This reverts commit 2786fcf213e7b4e9d86d4ad4dce7a10f018a9e66.
    
    Very sus
    
    Signed-off-by: Yahya Wessam <yahyawessam2002@gmail.com>

--- drivers/gpu/msm/kgsl_events.c
+++ drivers/gpu/msm/kgsl_events.c
@@ -1,7 +1,14 @@
-// SPDX-License-Identifier: GPL-2.0-only
-/*
- * Copyright (c) 2011-2019, The Linux Foundation. All rights reserved.
- * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+/* Copyright (c) 2011-2019, The Linux Foundation. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
  */
 
 #include <linux/slab.h>
@@ -25,17 +32,17 @@ static inline void signal_event(struct kgsl_device *device,
 {
 	list_del(&event->node);
 	event->result = result;
-	kthread_queue_work(device->events_worker, &event->work);
+	queue_work(device->events_wq, &event->work);
 }
 
 /**
  * _kgsl_event_worker() - Work handler for processing GPU event callbacks
- * @work: Pointer to the kthread_work for the event
+ * @work: Pointer to the work_struct for the event
  *
- * Each event callback has its own kthread_work struct and is run on a event specific
- * worker thread.  This is the worker that queues up the event callback function.
+ * Each event callback has its own work struct and is run on a event specific
+ * workqeuue.  This is the worker that queues up the event callback function.
  */
-static void _kgsl_event_worker(struct kthread_work *work)
+static void _kgsl_event_worker(struct work_struct *work)
 {
 	struct kgsl_event *event = container_of(work, struct kgsl_event, work);
 
@@ -275,7 +282,7 @@ int kgsl_add_event(struct kgsl_device *device, struct kgsl_event_group *group,
 	event->created = jiffies;
 	event->group = group;
 
-	kthread_init_work(&event->work, _kgsl_event_worker);
+	INIT_WORK(&event->work, _kgsl_event_worker);
 
 	trace_kgsl_register_event(KGSL_CONTEXT_ID(context), timestamp, func);
 
@@ -290,7 +297,7 @@ int kgsl_add_event(struct kgsl_device *device, struct kgsl_event_group *group,
 
 	if (timestamp_cmp(retired, timestamp) >= 0) {
 		event->result = KGSL_EVENT_RETIRED;
-		kthread_queue_work(device->events_worker, &event->work);
+		queue_work(device->events_wq, &event->work);
 		spin_unlock(&group->lock);
 		return 0;
 	}
